c59cb090b4728d60174414b22737949bcc2dca76
Nishanov Khasan <99598107+HasanNishanov@users.noreply.github.com>
2022-07-22 00:09:05 +0500

    good night x3

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,9 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
-        first = first > second ?
-                first:
-                second;
+        first = first > second ? first: (second>third?second:third);
         System.out.println(first);
 
 
